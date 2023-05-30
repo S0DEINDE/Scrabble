2b5f5f312aa4587e9e0da2b9b8f64e58da3fdeaf
David <odsodeinde@loyola.edu>
2023-05-30 05:09:10 -0500

    enjoy:p

--- Simon.py
+++ Simon.py
@@ -3,77 +3,77 @@ import random
 import time
 
 
-# Clear the game
+# Function to clear the game
 def clear_the_area():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-# Show High-score
+# Function to output new high-score
 def show_me():
     print(f"High Score: {highscore} by {highname}")
 
 
-# Show Player's Current Score
+# Function to output the current score of the user
 def show_current():
-    print(f"Score: {score}")
+    print(f"Score: {score_counter}")
 
 
-# Add Color to Simon's Sequence
+# Function to add another color to Simon's sequence
 def adder():
     global simons_colors
     simons_colors += random.choice(colors)
 
 
-# Your Turn
+# Function to say that is now your turn
 def user_time():
     turn = input("Your turn:\n").upper()
     return turn
 
 
-# Get High Score as Number
+# Function to get the current high-score
 def getter():
     with open("highscore.txt", "r") as hs:
         stats = hs.readlines()
     return stats
 
 
-# Set High Score and Name
+# Function to set new high-score
 def setter(n, s):
     with open("highscore.txt", "w") as hs:
         hs.write(f"{n}\n{s}")
 
 
-# Create highscore file if none
+# Try and except for using the file to reach the current high-score file
 try:
     getter()
 except:
     with open("highscore.txt", "w") as f:
         f.write("null\n0")
 
-# High Score Values
+# This is for current the high-score
 record = getter()
 highname = record[0]
 highscore = int(record[1])
 
-# Possible Colors in Sequence
+# The possible colors for Simon
 colors = ('R', 'G', 'B', 'Y')
 
-# Simon's Sequence
+# The list for Simon's colors
 simons_colors = []
 
-# Tracks Player's Score
-score = 0
+# The count for the user's current score
+score_counter = 0
 
 # Initialize Simon's Sequence
 for i in range(2):
     adder()
 
-# Show High Score
+# Output High Score
 clear_the_area()
 show_me()
 time.sleep(2)
 
-# Show Player Score
+# Output the user's current score
 clear_the_area()
 show_current()
 time.sleep(2)
@@ -81,27 +81,27 @@ time.sleep(2)
 # Gameplay Loop
 while True:
 
-    # Add Color to Sequence
+    # call the add color function
     adder()
 
-    # Combine Simon's Colors into Sequence
+    # addd the colors to Simon's sequence
     sequence = ''.join(simons_colors)
 
-    # Show Simon before printing sequence
+    # Output the official start of the game
     print("Simon says:")
     time.sleep(1)
     clear_the_area()
     show_current()
 
-    # Show One Color at a Time
+    # Output a color at a time
     for color in simons_colors:
-        # Pause Between Colors
+        # Make sure there is a pause for the user after each color Simon says
         print("Simon says:")
         time.sleep(0.1)
         clear_the_area()
         show_current()
 
-        # Display Next Color
+        # Output the next color Simon says
         print(f"Simon says: {color}")
         time.sleep(1)
         clear_the_area()
@@ -110,38 +110,38 @@ while True:
     # Correct Sequence from Player
     if user_time() == sequence:
 
-        # Increase Score
-        score += 1
+        # Increment the score
+        score_counter += 1
         clear_the_area()
         show_current()
 
-    # Game Over
+    # Game Over condition
     else:
 
-        # New High Score
-        if score > highscore:
+        # New High-score condition
+        if score_counter > highscore:
 
-            # Display High Score Message
+            # Output the new high-score
             clear_the_area()
-            print(f"GAME OVER\nNew High Score: {score}!")
+            print(f"GAME OVER\nNew High Score: {score_counter}!")
 
-            # Get player's name
+            # Ask user for input of their name
             name = input("Wass ya name? ")
 
-            # Save new high score
-            setter(name, str(score))
+            # call setter function
+            setter(name, str(score_counter))
             break
 
-        # No New High Score
+        # No New High Score Condition
         else:
 
-            # Display Game Over Message
+            # Output Game Over Message
             clear_the_area()
             print("GAME OVER")
 
-            # Show High Score
+            # Output High-score
             show_me()
 
-            # Show Player Score
-            print(f"Score: {score}")
+            # Output player score
+            print(f"Score: {score_counter}")
             break
